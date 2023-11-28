cb590442865c52c6eb8fed5025b36af8618c0b4e
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-28 11:16:46 -0300

    add empty pre scores

--- src/components/ScoresView.jsx
+++ src/components/ScoresView.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState,useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import '@styles/components/ScoresView.scss'
 import { variableContext } from '../context/context.jsx'
 
 let data;
 const ScoreView = (prop) => {
     const contexto = useContext(variableContext)
-    const [getData, setData] = useState([])
+    const [getData, setData] = useState([
+        {},{},{},{},{},{},{},{},{},{},
+    ])
     useEffect(() => {
-        if(contexto.getCompletedGame && data==undefined ){
+        if (contexto.getCompletedGame && data == undefined) {
             console.log('GET-Request')
             async function getApiScores() {
                 const res = await fetch(`https://db-asdjosegarcia.vercel.app/api/jigsaw/scores/best-ten/${contexto.getScore.map}`)
                 /* const */ data = (await res.json())
                 console.log(await data)
                 setData(await data)
-                data=undefined
+                data = undefined
             }
             getApiScores()
         }
@@ -37,14 +39,15 @@ const ScoreView = (prop) => {
                 </thead>
                 <tbody>
                     {/* {console.log(getData)} */}
-                    {  getData.map((user,index)=>{return(<tr key={user.id}>{<td>{index+1}</td>}<td>{user.user}</td><td>{user.secondsPlayed}</td><td>{user.movementsNumber}</td><td>{user.score}</td></tr>)})}  
-                    {/*                     <tr>
-                        <td>Juan</td>
-                        <td>Gomez</td>
-                        <td>32</td>
-                        <td>328382</td>
-                    </tr>
-    */}
+                    {getData.map((user, index) => { return (<tr key={index}>{<td>{index + 1}</td>}<td>{user.user}</td><td>{user.secondsPlayed}</td><td>{user.movementsNumber}</td><td>{user.score}</td></tr>) })}
+{/*                     <tr>
+                        <td>----</td>
+                        <td>----</td>
+                        <td>----</td>
+                        <td>----</td>
+                        <td>----</td>
+                    </tr> */}
+
                 </tbody>
             </table>
         </div>
